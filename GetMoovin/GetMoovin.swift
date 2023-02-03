bda25fac29a8de9e0737399d6c224c374a2a9130
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-02-03 08:28:25 -0800

    Initial Project Setup (#1)

---
+++ GetMoovin/GetMoovin.swift
@@ -0,0 +1,31 @@
+//
+// This source file is part of the CS342 2023 GetMoovin Team Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import CardinalKit
+import GetMoovinOnboardingFlow
+import GetMoovinSharedContext
+import SwiftUI
+
+
+@main
+struct GetMoovin: App {
+    @UIApplicationDelegateAdaptor(GetMoovinAppDelegate.self) var appDelegate
+    @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .sheet(isPresented: !$completedOnboardingFlow) {
+                    OnboardingFlow()
+                }
+                .testingSetup()
+                .cardinalKit(appDelegate)
+        }
+    }
+}
