3cefced386b330ed38a5b6974fd0d72670f35b05
parthavshergill <110453542+parthavshergill@users.noreply.github.com>
2023-03-01 23:04:13 -0800

    Daily Progress + Long Term Progress Views (#13)
    Co-authored-by: anushehchaudry <103084605+anushehchaudry@users.noreply.github.com>

---
+++ GetMoovin/LongtermProgress/LongTermProgressChartView.swift
@@ -0,0 +1,75 @@
+//
+//  LongTermProgressChartView.swift
+//  GetMoovin
+//
+//  Created by Parthav Shergill on 28/02/23.
+//
+
+import Charts
+import GetMoovinSharedContext
+import GetMoovinStepCountModule
+import SwiftUI
+
+struct LongTermProgressChartView: View {
+    @EnvironmentObject var stepCountDataSource: StepCountDataSource
+    @AppStorage(StorageKeys.userInformation) var userInformation = UserInformation()
+    
+    let stepMonths: [StepMonth] = [
+        .init(date: Date.from(year: 2022, month: 1, day: 1), stepCount: 2000),
+        .init(date: Date.from(year: 2022, month: 2, day: 1), stepCount: 3000),
+        .init(date: Date.from(year: 2022, month: 3, day: 1), stepCount: 4000),
+        .init(date: Date.from(year: 2022, month: 4, day: 1), stepCount: 5000),
+        .init(date: Date.from(year: 2022, month: 5, day: 1), stepCount: 11000)
+    ]
+    
+    
+    var body: some View {
+        VStack {
+            Chart {
+                ForEach(stepMonths) { stepMonth in
+                    BarMark(
+                        x: .value("Month", stepMonth.date, unit: .month),
+                        y: .value("Views", stepMonth.stepCount)
+                    )
+                    .foregroundStyle(Color.pink.gradient)
+                }
+                
+                RuleMark(y: .value("Goal", 10000))
+                    .foregroundStyle(Color.mint)
+                    .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
+                    .annotation(alignment: .leading) {
+                        Text("Goal")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+            }
+            .frame(height: 300)
+            .chartXAxis {
+                AxisMarks(values: stepMonths.map { $0.date }) { date in
+                    AxisGridLine()
+                    AxisValueLabel(format: .dateTime.month())
+                }
+            }
+        }
+    }
+}
+
+struct StepMonth: Identifiable {
+    let id = UUID()
+    let date: Date
+    let stepCount: Int
+}
+
+extension Date {
+    static func from(year: Int, month: Int, day: Int) -> Date {
+        let components = DateComponents(year: year, month: month, day: day)
+        return Calendar.current.date(from: components)!
+    }
+}
+
+
+struct LongTermProgressChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        LongTermProgressChartView()
+    }
+}
