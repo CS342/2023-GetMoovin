629fa479d02f90e21005e1042e6f580f4906a159
sabarrag <102919179+sabarrag@users.noreply.github.com>
2023-03-12 17:18:23 -0700

    added license tto Change Goal view

--- GetMoovin/TodaysGoal/ChangeGoal.swift
+++ GetMoovin/TodaysGoal/ChangeGoal.swift
@@ -1,8 +1,9 @@
 //
-//  ChangeGoal.swift
-//  GetMoovin
+// This source file is part of the CS342 2023 GetMoovin Team Application project
 //
-//  Created by Sarah Barragan on 3/12/23.
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
 //
 import GetMoovinSharedContext
 import Onboarding
